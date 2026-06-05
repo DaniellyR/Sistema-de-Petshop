b1b8c08e952d8955444ab3735e7c92b27d92d0c2
Danielly Reis <reisdanny209@gmail.com>
2026-06-05 16:14:05 -0300

    fix: correções gerais

--- petshop/rotaspets.js
+++ petshop/rotaspets.js
@@ -1,6 +1,9 @@
 const conexao = require('./banco');
 
-function listarPets(req, res) {
+
+// CRUD
+
+function listarPets(req, res) { // função para ler dentro do BD a tela pets; ler (R)
     conexao.query('select * from pets', (err, resultados) => {
         if(err){
             console.log('Erro ao acessar tabela');
@@ -12,4 +15,79 @@ function listarPets(req, res) {
     });
 }
 
-module.exports = { listarPets };
+function cadastrarPet(req, res) {   //receber novos pets no BD  crearte C
+    let corpo = ''; //as info
+
+    req.on('data', (pedaco) => {
+        corpo += pedaco; 
+    });
+
+    req.on('end', () => { //conexão com o banco
+        const novoPet = JSON.parse(corpo); 
+        const sql = 'INSERT INTO pets (nome, especie, idade, sexo, ID_tutor) VALUES (?, ?, ?, ?, ?)';
+        const valores = [novoPet.nome, novoPet.especie, novoPet.idade, novoPet.sexo, novoPet.ID_tutor];
+
+        conexao.query(sql, valores, (err, resultados) => {
+            if(err){
+                console.log('Erro ao cadastrar pet no banco', err);
+                res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+                res.end('Erro interno ao cadastrar');
+            } else {
+                res.writeHead(201, { 'Content-Type': 'text/plain; charset=utf-8' });
+                res.end('Pet salvo no banco de dados com sucesso!');
+            }
+        });
+    });
+}
+
+function deletePets(req, res){ //deletar D
+    let corpo = '';
+
+    req.on('data', (pedaco) => {
+        corpo += pedaco;
+    });
+
+    req.on('end', () => {
+        const dados = JSON.parse(corpo); 
+        const sql = 'DELETE FROM pets WHERE ID_pets = ?';
+        const valores = [dados.ID_pets];
+
+        conexao.query(sql, valores, (err, resultados) => {
+            if(err){
+                console.log('Erro ao deletar pet', err);
+                res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+                res.end('Erro interno ao tentar apagar o pet');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+                res.end('Pet deletado do sistema com sucesso!');
+            }
+        });
+    });
+    
+}
+
+function atualizarPet(req, res){ // U de update
+    let corpo = '';
+
+    req.on('data', (pedaco) => {
+        corpo += pedaco;
+    });
+    req.on('end', () => {
+        const atualizacao = JSON.parse(corpo);    
+        const sql = 'UPDATE pets SET nome = ?, especie = ?, idade = ?, sexo = ?, ID_tutor = ? WHERE ID_pets = ?';;
+        const valores = [atualizacao.nome, atualizacao.especie, atualizacao.idade, atualizacao.sexo, atualizacao.ID_tutor, atualizacao.ID_pets];
+
+    conexao.query(sql, valores, (err, resultados) => {
+        if(err){
+            console.log('Erro ao atualizar pet', err);
+            res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+            res.end('Erro interno ao tentar atualizar o pet');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+            res.end('Pet atualizado no sistema com sucesso!');
+        }
+    });
+});
+}
+
+module.exports = { listarPets, cadastrarPet, deletePets, atualizarPet };
--- petshop/server.js
+++ petshop/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const http = require('http');
 const conexao = require('./banco');
 const rotasPets = require('./rotasPets');
@@ -7,13 +8,78 @@ const servidor = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end('Página de Login do Petshop');
     }
-    else if ('/' === req.url){
-        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end("Bem vindo ao seu Petshop");
+    else if ('/' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/index.html', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(pagina);
+        });
     }
-    else if ('/pets' == req.url){
-        rotasPets.listarPets(req, res); 
+    else if ('/agendamento' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/agendamento.html', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(pagina);
+        });
     }
+
+    else if ('/confirmacao' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/confirmacao.html', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(pagina);
+        });
+    }
+    else if ('/style.css' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/style.css', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'text/css; charset=utf-8' });
+            res.end(pagina);
+        });
+    }
+
+    //imagens
+    else if ('/icone-pata.png' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/icone-pata.png', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'image/png; charset=utf-8' });
+            res.end(pagina);
+        });
+    }
+    else if ('/icone-geral.png' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/icone-geral.png', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'image/png; charset=utf-8' });
+            res.end(pagina);
+        });
+    }
+    else if ('/icone-tesoura.png' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/icone-tesoura.png', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'image/png; charset=utf-8' });
+            res.end(pagina);
+        });
+    }
+    else if ('/icone-animais.webp' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/icone-animais.webp', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'image/webp; charset=utf-8' });
+            res.end(pagina);
+        });
+    }
+    else if ('/icone-banheira.webp' === req.url && 'GET' === req.method) {
+        fs.readFile('./site/icone-banheira.webp', (err, pagina) => {
+            res.writeHead(200, { 'Content-Type': 'image/webp; charset=utf-8' });
+            res.end(pagina);
+        });
+    }
+
+    //tabela pets
+    else if ('/pets' === req.url && 'GET' === req.method) {
+        rotasPets.listarPets(req, res);
+    }
+    else if ('/pets' === req.url && 'POST' === req.method) {
+        rotasPets.cadastrarPet(req, res);
+    }
+    else if ('/pets' === req.url && 'DELETE' == req.method){
+        rotasPets.deletarPet(req, res);
+    }
+    else if ('/pets' === req.url && 'PUT' === req.method){
+        rotasPets.atualizarPet(req, res);
+    }
+
     else if ('/admins' == req.url){
         conexao.query('select * from administrador', (err, resultados) => {
             if(err){
